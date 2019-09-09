ee805ae1fd9b3b1048af161b259380f563432116
Anthony Oglesby <ogle1ant@gmail.com>
2019-09-09 15:21:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,16 +1,16 @@
 class School
   attr_accessor :name, :roster
-  
-  def initialize
+
+  def initialize(name)
     @name = name
     @roster = {}
   end
-  
+
   def add_student(student_name, grade)
     roster[grade] ||= []
     roster[grade] << student_name
   end
-  
+
   def grade(student_grade)
     roster[student_grade]
   end
