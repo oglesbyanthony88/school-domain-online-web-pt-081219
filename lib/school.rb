c939aa342e9b119cd8c9770e82bfb8bde4333471
Anthony Oglesby <ogle1ant@gmail.com>
2019-09-09 15:20:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,6 +15,13 @@ class School
     roster[student_grade]
   end
   
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+    
   
     
     
